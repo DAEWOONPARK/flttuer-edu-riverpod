c9d50004072118c95a90da3595c2e2702d7a7373
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2022-12-08 16:21:49 +0900

    Family Modifier 실습

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_sample/layout/default_layout.dart';
+import 'package:riverpod_sample/screen/family_modifier_screen.dart';
 import 'package:riverpod_sample/screen/future_provider_screen.dart';
 import 'package:riverpod_sample/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_sample/screen/state_provider_screen.dart';
@@ -45,6 +46,14 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: Text('StreamProviderScreen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (_) => FamilyModifierScreen(),),
+                );
+              },
+              child: Text('FamilyModifierScreen'),
             ),
           ],
         ));
