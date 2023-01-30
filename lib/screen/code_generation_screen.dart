16bdbaaf6a8f8e4da2b428d2c5c3d5b043dcf447
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2023-01-30 16:46:57 +0900

    Riverpod v2 Family로 파라미터 전달

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -12,6 +12,7 @@ class CodeGenerationScreen extends ConsumerWidget {
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
     final state3 = ref.watch(gStateFuture2Provider);
+    final state4 = ref.watch(gStateMultiplyProvider(number1: 10, number2: 20));
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
@@ -41,7 +42,8 @@ class CodeGenerationScreen extends ConsumerWidget {
               err.toString(),
             ),
             loading: () => Center(child: CircularProgressIndicator()),
-          )
+          ),
+          Text('State4: $state4'),
         ],
       ),
     );
